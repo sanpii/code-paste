1d6dca0fb8fa4453c088d0b3d420fdfa31228b95
Sanpi <sanpi@homecomputing.fr>
2013-01-09 16:45:00 +0100

    Add snippet

--- src/app.php
+++ src/app.php
@@ -4,8 +4,28 @@ use \Symfony\Component\HttpFoundation\Request;
 
 $app = require __DIR__ . '/bootstrap.php';
 
-$app->get('/', function(Request $request) use($app) {
-    return $app['twig']->render('index.html.twig');
+$app->get('/add', function() use($app) {
+    $snippet = $app['pomm']->getMapFor('\Model\Snippet')
+        ->createObject(array(
+            'title' => '',
+            'code' => '',
+            'keywords' => array(),
+            'language' => 'text',
+        ));
+
+    return $app['twig']->render(
+        'add.html.twig',
+        array(
+            'snippet' => $snippet->extract(),
+            'languages' => array('php', 'text'),
+        )
+    );
+});
+
+$app->post('/add', function(Request $request) use($app) {
+    $snippet = $app['pomm']->getMapFor('\Model\Snippet')
+        ->createAndSaveObject($request->request->get('snippet'));
+    return $app->redirect("/show/{$snippet->id}");
 });
 
 return $app;
