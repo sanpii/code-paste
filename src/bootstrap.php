7051e9e4edda8199cd2ad305b8eb0e63bddc698c
Sanpi <sanpi@homecomputing.fr>
2014-11-26 12:16:34 +0100

    Update bootstrap

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,10 +1,10 @@
 <?php
 
-use \Pomm\Silex\PommServiceProvider;
-use \Silex\Provider\TwigServiceProvider;
-use \Silex\Provider\WebProfilerServiceProvider;
-use \Silex\Provider\UrlGeneratorServiceProvider;
-use \Silex\Provider\ServiceControllerServiceProvider;
+use \Silex\Provider;
+use \PommProject\Silex\ {
+    ServiceProvider\PommServiceProvider,
+    ProfilerServiceProvider\PommProfilerServiceProvider
+};
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
@@ -12,38 +12,41 @@ if (!is_file(__DIR__ . '/config/current.php')) {
     throw new \RunTimeException('No current configuration file found in config.');
 }
 
-$app = new Silex\Application();
+$app = new \Silex\Application();
 
-$app['config'] = require __DIR__ . '/config/current.php';
+$app['config'] = function () use($app) {
+    $config = require __DIR__ . '/config/current.php';
+    $config['pomm.configuration']['code']['class:session_builder'] = '\Model\SessionBuilder';
+
+    return $config;
+};
 
 $app['debug'] = $app['config']['debug'];
 
-$app->register(new TwigServiceProvider(), [
+$app->register(new Provider\TwigServiceProvider, [
     'twig.path' => __DIR__ . '/views',
 ]);
 
-$databaseConfig = $app['config']['pomm'];
-foreach ($databaseConfig as $name => $values) {
-    $databaseConfig[$name]['class'] = '\Model\Database';
-}
-$app->register(new PommServiceProvider(), [
-    'pomm.class_path' => __DIR__ . '/vendor/pomm',
-    'pomm.databases' => $databaseConfig,
-]);
+$app->register(new PommServiceProvider, $app['config']);
+
+$app['db'] = function () use($app) {
+    return $app['pomm']['code'];
+};
 
 $app['geshi'] = function() use ($app) {
     return new GeSHi();
 };
 
 if (class_exists('\Silex\Provider\WebProfilerServiceProvider')) {
-    $app->register(new UrlGeneratorServiceProvider());
-    $app->register(new ServiceControllerServiceProvider());
+    $app->register(new Provider\ServiceControllerServiceProvider);
+    $app->register(new Provider\HttpFragmentServiceProvider);
 
-    $profiler = new WebProfilerServiceProvider();
-    $app->register($profiler, [
+    $app->register(new Provider\WebProfilerServiceProvider, [
         'profiler.cache_dir' => __DIR__ . '/../cache/profiler',
+        'profiler.mount_prefix' => '/_profiler',
     ]);
-    $app->mount('/_profiler', $profiler);
+
+    $app->register(new PommProfilerServiceProvider);
 }
 
 return $app;
