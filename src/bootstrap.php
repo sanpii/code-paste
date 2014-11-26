f8f7300bf89708e5e414198d19ddd763cb7dd43c
Sanpi <sanpi@homecomputing.fr>
2014-11-26 12:16:34 +0100

    Update bootstrap

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,10 +1,10 @@
 <?php
 
-use \Pomm\Silex\PommServiceProvider;
 use \Silex\Provider\TwigServiceProvider;
 use \Silex\Provider\WebProfilerServiceProvider;
 use \Silex\Provider\UrlGeneratorServiceProvider;
 use \Silex\Provider\ServiceControllerServiceProvider;
+use \PommProject\Silex\ServiceProvider\PommServiceProvider;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
@@ -12,9 +12,14 @@ if (!is_file(__DIR__ . '/config/current.php')) {
     throw new \RunTimeException('No current configuration file found in config.');
 }
 
-$app = new Silex\Application();
+$app = new \Silex\Application();
 
-$app['config'] = require __DIR__ . '/config/current.php';
+$app['config'] = $app->share(function () use($app) {
+    $config = require __DIR__ . '/config/current.php';
+    $config['pomm.configuration']['code']['class:session_builder'] = '\Model\SessionBuilder';
+
+    return $config;
+});
 
 $app['debug'] = $app['config']['debug'];
 
